fd68283fbff273c2681274009d7cb69e52cfd0da
Carlos Gonzalez <homearanya@gmail.copm>
2022-09-23 13:20:54 +0200

    remove gatsby-plugin-netlify-identity-widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,7 +142,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    "gatsby-plugin-netlify-identity-widget",
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
   mapping: {
